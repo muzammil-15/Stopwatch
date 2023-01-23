559d4ccdc83b0845952e24811e2a2f9e0c68af34
muzammil-15 <121623043+muzammil-15@users.noreply.github.com>
2023-01-23 22:33:18 +0500

    Add files via upload

---
+++ stopwatch/script.js
@@ -0,0 +1,78 @@
+var hr = 00;
+var min = 00;
+var sec = 00;
+var count = 00;
+
+var timer = false;
+
+function start(){
+    timer = true;
+    stopwatch();
+}
+
+function stop(){
+    timer = false;
+}
+
+function reset(){
+    timer = false;
+
+    hr = 0;
+    min = 0;
+    sec = 0;
+    count = 0;
+
+    document.getElementById("hr").innerHTML = "00";
+    document.getElementById("min").innerHTML = "00";
+    document.getElementById("sec").innerHTML = "00";
+    document.getElementById("count").innerHTML = "00";
+
+}
+
+function stopwatch(){
+    if(timer == true){
+        count = count+1;
+        if(count == 100){
+            sec = sec+1;
+            count = 0;
+        }
+
+        if(sec == 60){
+            min = min+1;
+            sec = 0;
+        }
+
+        if(min == 60){
+            hr = hr+1;
+            min = 0;
+            sec = 0;
+        }
+
+        var hrstring = hr;
+        var minstring = min;
+        var secstring = sec;
+        var countstring = count;
+
+        if (hr < 10){
+            hrstring = "0" + hrstring
+        }
+        if (min < 10){
+            minstring = "0" + minstring
+        }
+        if (sec < 10){
+            secstring = "0" + secstring
+        }
+        if (count < 10){
+            countstring = "0" + countstring
+        }
+
+
+
+        document.getElementById("hr").innerHTML = hrstring;
+        document.getElementById("min").innerHTML = minstring;
+        document.getElementById("sec").innerHTML = secstring;
+        document.getElementById("count").innerHTML = countstring;
+
+        setTimeout( "stopwatch()", 10)
+    }
+}
